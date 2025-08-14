8b387a41b63a8c03dfc1ab8bb4e8b0c2467f82f8
Aleksandr Zalesov <webdenval@gmail.com>
2025-08-14 21:48:45 +0500

    HW3

---
+++ HW3/A12.c
@@ -0,0 +1,34 @@
+/*
+ * A12.c
+ * 
+ * Copyright 2025 webde <webde@DS-LAPTOP>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+#include <stdio.h>
+
+int main()
+{
+	int x;
+	scanf("%d",&x);
+	printf("%d\n",x/100+x%100/10+x%10);
+	return 0;
+}
+
---
+++ HW3/A4.c
@@ -0,0 +1,35 @@
+/*
+ * A4.c
+ * 
+ * Copyright 2025 webde <webde@DS-LAPTOP>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+#include <stdio.h>
+
+int main()
+{
+	int a,b,c;
+	scanf("%d%d%d",&a,&b,&c);
+	printf("%d+%d+%d=%d\n",a,b,c,a+b+c);
+	printf("%d*%d*%d=%d",a,b,c,a*b*c);
+	return 0;
+}
+
